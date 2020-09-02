390bef128a6916ee8702ed833e3b4a26700162c6
bcool001 <69883333+bcool001@users.noreply.github.com>
2020-09-02 17:51:19 +1000

    same

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+        <head lang=en>
+            <title> Online Banking System </title>
+            <link rel="stylesheet" type="text/css" href="css/Style.css">   
+        </head>
+            <body>
+                
+                <h2  id=top_text> WELCOME TO ONLINE BANKING</h2>
+            <div class="login-box">
+            <img src="images/avatar.png" class="avatar">
+                <h1>Login Here</h1>
+                    <form>
+                    <p>Username</p>
+                    <input type="text" name="username" placeholder="Enter Username">
+                    <p>Password</p>
+                    <input type="password" name="password" placeholder="Enter Password">
+                    <input type="submit" name="submit" value="Login">
+                    <a href="forgot%20password.html">forgot password</a>    </br></br>
+                        <a href="create account.html">Create Account</a>    
+                    </form>
+                
+                
+                </div>
+            
+            </body>
+        </html>
